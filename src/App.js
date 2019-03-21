9e949729d16a59c5b1ee49cc15ce947f5e16203a
Junaid <junaidchoudhary210@gmail.com>
2019-03-21 23:34:08 +0530

    Current params done

--- src/App.js
+++ src/App.js
@@ -3,29 +3,44 @@ import './App.css';
 
 class App extends Component {
 
-  callApi = async () => {
-    const url = 'https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=605fd8751dfa134aa2b8287ca0aa650e'
+  state = {
+    currentTemp: '',
+    currentHum: '',
+    currentPrec: ''
+  }
+
+  callApi = async (city) => {
+    const apiKey = '605fd8751dfa134aa2b8287ca0aa650e'
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}`
     const response = await fetch(url)
-    const body = await response.json()
-    console.log(body)
+    const data = await response.json()
+    const currentParams = data.list[0]
+    const currentTemp = currentParams.main.temp
+    const currentHum = currentParams.main.humidity
+    const currentPrec = currentParams.weather[0].description
+    this.setState({currentTemp: currentTemp, currentHum: currentHum, currentPrec: currentPrec})
   }
 
   submitForm = (e) => {
     e.preventDefault()
     const city = e.target.name.value
+    this.callApi(city)
   }
 
 
   render() {
+    const currentParams = (this.state.currentTemp && 
+      <h2>Current Temperature: {this.state.currentTemp}, Humidity: {this.state.currentHum}, Precipitation(mode): {this.state.currentPrec}</h2>)
     return (
       <div className="App">
       <form style={{margin: '10% 40%'}} onSubmit={this.submitForm}>
         <div className="input-field">
           <input name="name" type="text" id="name" />
-          <label for="name">Your name:</label>
+          <label>Your name:</label>
           <button className="buttonfx angleinleft bouncein" type="submit">Submit</button>
         </div>
       </form>
+      {currentParams ? currentParams: <h2>Current Params will go Here...</h2>}
       </div>
     );
   }
