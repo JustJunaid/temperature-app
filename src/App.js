cc8f4d329bed59afeab4701919f5a1006433afed
Junaid <junaidchoudhary210@gmail.com>
2019-03-21 22:34:51 +0530

    started with temperature app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  callApi = async () => {
+    const url = 'https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=605fd8751dfa134aa2b8287ca0aa650e'
+    const response = await fetch(url)
+    const body = await response.json()
+    console.log(body)
+  }
+
+  submitForm = (e) => {
+    e.preventDefault()
+    const city = e.target.name.value
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <form style={{margin: '10% 40%'}} onSubmit={this.submitForm}>
+        <div className="input-field">
+          <input name="name" type="text" id="name" />
+          <label for="name">Your name:</label>
+          <button className="buttonfx angleinleft bouncein" type="submit">Submit</button>
+        </div>
+      </form>
       </div>
     );
   }
